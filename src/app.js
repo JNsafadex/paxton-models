579ea8e72e3138e7598a0ea222b6ed1c5e52bd82
Jeffrey Epstein <ravaianomiguel@gmail.com>
2025-11-11 11:17:32 +0000

    inicialização do servidor

---
+++ src/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('node:path');
+var app = express()
+app.use(express.static(path.join(__dirname,"..", 'public')));
+app.listen(3000);
+console.log('listening on port 3000');
+
